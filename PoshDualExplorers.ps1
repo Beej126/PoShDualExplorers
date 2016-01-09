876968eec5b32a5b73f794a0c7512476d9193d3b
Beej <beej126@hotmail.com>
2016-01-08 18:12:54 -0800

    added couple more command buttons - open selected folder in new tab

--- PoshDualExplorers.ps1
+++ PoshDualExplorers.ps1
@@ -25,148 +25,166 @@
 #   http://blog.backslasher.net/copying-files-in-powershell-using-windows-explorer-ui.html
 #   http://stackoverflow.com/questions/8292953/get-current-selection-in-windowsexplorer-from-a-c-sharp-application
 
+$Error.Clear()
+
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
+Add-Type -Path $PSScriptRoot\Interop.SHDocVw.dll
+
 # https://github.com/denwilliams/FontAwesome-WindowsForms
-Add-Type -path FontAwesomeIcons.dll 
+Add-Type -path $PSScriptRoot\FontAwesomeIcons.dll 
 
-
-Add-Type @"
+#save the compiled user32 exports wrapper to see if we can spin up faster... in my testing it didn't seem to make a difference
+if (Test-path $PSScriptRoot\user32.dll) { Add-Type -path $PSScriptRoot\user32.dll }
+else {
+Add-Type -OutputAssembly $PSScriptRoot\user32.dll -TypeDefinition @"
 
 using System;
 using System.Runtime.InteropServices;
 using System.Text;
 
-    public class Win32 {
+public class User32 {
 
-        public static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
-        public static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
-        public static readonly IntPtr HWND_TOP = new IntPtr(0);
-        public static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+    public static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+    public static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+    public static readonly IntPtr HWND_TOP = new IntPtr(0);
+    public static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
 
-        public static readonly int
-            NOSIZE = 0x0001,
-            NOMOVE = 0x0002,
-            NOZORDER = 0x0004,
-            NOREDRAW = 0x0008,
-            NOACTIVATE = 0x0010,
-            DRAWFRAME = 0x0020,
-            FRAMECHANGED = 0x0020,
-            SHOWWINDOW = 0x0040,
-            HIDEWINDOW = 0x0080,
-            NOCOPYBITS = 0x0100,
-            NOOWNERZORDER = 0x0200,
-            NOREPOSITION = 0x0200,
-            NOSENDCHANGING = 0x0400,
-            DEFERERASE = 0x2000,
-            ASYNCWINDOWPOS = 0x4000,
-            GWL_STYLE = -16,
-            GWL_EXSTYLE = -20,
-            WM_SYSCOMMAND = 0x0112,
-            SC_CLOSE = 0xF060,
+    public static readonly int
+        NOSIZE = 0x0001,
+        NOMOVE = 0x0002,
+        NOZORDER = 0x0004,
+        NOREDRAW = 0x0008,
+        NOACTIVATE = 0x0010,
+        DRAWFRAME = 0x0020,
+        FRAMECHANGED = 0x0020,
+        SHOWWINDOW = 0x0040,
+        HIDEWINDOW = 0x0080,
+        NOCOPYBITS = 0x0100,
+        NOOWNERZORDER = 0x0200,
+        NOREPOSITION = 0x0200,
+        NOSENDCHANGING = 0x0400,
+        DEFERERASE = 0x2000,
+        ASYNCWINDOWPOS = 0x4000,
+        GWL_STYLE = -16,
+        GWL_EXSTYLE = -20,
+        WM_SYSCOMMAND = 0x0112,
+        SC_CLOSE = 0xF060,
 
-            WS_BORDER = 0x00800000,
-            WS_DLGFRAME = 0x00400000,
-            WS_CAPTION = WS_BORDER | WS_DLGFRAME,
-            WS_SYSMENU = 524288,
-            WS_THICKFRAME = 262144,
-            WS_MINIMIZE = 536870912,
-            WS_MAXIMIZEBOX = 65536,
+        WS_BORDER = 0x00800000,
+        WS_DLGFRAME = 0x00400000,
+        WS_CAPTION = WS_BORDER | WS_DLGFRAME,
+        WS_SYSMENU = 524288,
+        WS_THICKFRAME = 262144,
+        WS_MINIMIZE = 536870912,
+        WS_MAXIMIZEBOX = 65536,
             
-            SWP_NOMOVE = 0x2,
-            SWP_NOSIZE = 0x1,
-            SWP_FRAMECHANGED = 0x20,
-            MF_BYPOSITION = 0x400,
-            MF_REMOVE = 0x1000,
+        SWP_NOMOVE = 0x2,
+        SWP_NOSIZE = 0x1,
+        SWP_FRAMECHANGED = 0x20,
+        MF_BYPOSITION = 0x400,
+        MF_REMOVE = 0x1000,
             
-            // Values for ShowWindowAsync(nCmdShow)
-            SW_HIDE = 0, 
-            SW_SHOWNORMAL = 1, 
-            SW_NORMAL = 1, 
-            SW_SHOWMINIMIZED = 2, 
-            SW_SHOWMAXIMIZED = 3, 
-            SW_MAXIMIZE = 3, 
-            SW_SHOWNOACTIVATE = 4, 
-            SW_SHOW = 5, 
-            SW_MINIMIZE = 6, 
-            SW_SHOWMINNOACTIVE = 7, 
-            SW_SHOWNA = 8, 
-            SW_RESTORE = 9, 
-            SW_SHOWDEFAULT = 10, 
-            SW_MAX = 10;
+        // Values for ShowWindowAsync(nCmdShow)
+        SW_HIDE = 0, 
+        SW_SHOWNORMAL = 1, 
+        SW_NORMAL = 1, 
+        SW_SHOWMINIMIZED = 2, 
+        SW_SHOWMAXIMIZED = 3, 
+        SW_MAXIMIZE = 3, 
+        SW_SHOWNOACTIVATE = 4, 
+        SW_SHOW = 5, 
+        SW_MINIMIZE = 6, 
+        SW_SHOWMINNOACTIVE = 7, 
+        SW_SHOWNA = 8, 
+        SW_RESTORE = 9, 
+        SW_SHOWDEFAULT = 10, 
+        SW_MAX = 10;
 
-        public static readonly uint
-            WS_POPUP = 0x80000000,
-            WS_CHILD = 0x40000000;
+    public static readonly uint
+        WS_POPUP = 0x80000000,
+        WS_CHILD = 0x40000000;
 
-        public static readonly long
-            WS_EX_DLGMODALFRAME = 0x1L;
+    public static readonly long
+        WS_EX_DLGMODALFRAME = 0x1L;
 
 
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        public static extern IntPtr FindWindow(String sClassName, String sAppName);
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    public static extern IntPtr FindWindow(String sClassName, String sAppName);
 
-        [DllImport("user32.dll")]
-        public static extern int SendMessage(int hWnd, uint Msg, int wParam, int lParam);
+    [DllImport("user32.dll")]
+    public static extern int SendMessage(int hWnd, uint Msg, int wParam, int lParam);
 
-        [DllImport("kernel32.dll")]
-        public static extern uint GetLastError();
+    [DllImport("kernel32.dll")]
+    public static extern uint GetLastError();
 
-        [DllImport("user32.dll")]
-        public static extern IntPtr SetParent(IntPtr hWnd,IntPtr hWndNewParent);
+    [DllImport("user32.dll")]
+    public static extern IntPtr SetParent(IntPtr hWnd,IntPtr hWndNewParent);
 
-        [DllImport("user32.dll")]
-        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+    [DllImport("user32.dll")]
+    public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
 
-        [DllImport("user32.dll")]
-        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    [DllImport("user32.dll")]
+    public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
-        [DllImport("user32.dll")]
-        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+    [DllImport("user32.dll")]
+    public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
 
-        //from here: http://stackoverflow.com/questions/2825528/removing-the-title-bar-of-external-application-using-c-sharp
-        public static void HideTitleBar(IntPtr hWnd) {
-            var style = GetWindowLong(hWnd, GWL_STYLE);
-            style = style & ~WS_CAPTION;
-            style = style & ~WS_SYSMENU;
-            style = style & ~WS_THICKFRAME;
-            style = style & ~WS_MINIMIZE;
-            style = style & ~WS_MAXIMIZEBOX;
-            SetWindowLong(hWnd, GWL_STYLE, style);
-        }
-
-        [DllImport("user32.dll")] 
-        public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
-
-        public enum EXTENDED_NAME_FORMAT
-        {
-            NameUnknown = 0,
-            NameFullyQualifiedDN = 1,
-            NameSamCompatible = 2,
-            NameDisplay = 3,
-            NameUniqueId = 6,
-            NameCanonical = 7,
-            NameUserPrincipal = 8,
-            NameCanonicalEx = 9,
-            NameServicePrincipal = 10,
-            NameDnsDomain = 12
-        }
-
-        [DllImport("secur32.dll", CharSet=CharSet.Auto, SetLastError=true)]
-        [return: MarshalAs(UnmanagedType.I4)]
-        public static extern int GetUserNameEx (EXTENDED_NAME_FORMAT nameFormat, StringBuilder userName, ref uint userNameSize);
+    //from here: http://stackoverflow.com/questions/2825528/removing-the-title-bar-of-external-application-using-c-sharp
+    public static void HideTitleBar(IntPtr hWnd) {
+        var style = GetWindowLong(hWnd, GWL_STYLE);
+        style = style & ~WS_CAPTION;
+        style = style & ~WS_SYSMENU;
+        style = style & ~WS_THICKFRAME;
+        style = style & ~WS_MINIMIZE;
+        style = style & ~WS_MAXIMIZEBOX;
+        SetWindowLong(hWnd, GWL_STYLE, style);
     }
-"@
+
+    [DllImport("user32.dll")] 
+    public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+
+    public enum EXTENDED_NAME_FORMAT
+    {
+        NameUnknown = 0,
+        NameFullyQualifiedDN = 1,
+        NameSamCompatible = 2,
+        NameDisplay = 3,
+        NameUniqueId = 6,
+        NameCanonical = 7,
+        NameUserPrincipal = 8,
+        NameCanonicalEx = 9,
+        NameServicePrincipal = 10,
+        NameDnsDomain = 12
+    }
+
+    [DllImport("secur32.dll", CharSet=CharSet.Auto, SetLastError=true)]
+    [return: MarshalAs(UnmanagedType.I4)]
+    public static extern int GetUserNameEx (EXTENDED_NAME_FORMAT nameFormat, StringBuilder userName, ref uint userNameSize);
+
+    [DllImport("user32.dll", CharSet=CharSet.Auto)]
+    public static extern bool IsWindowVisible(IntPtr hWnd);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SetForegroundWindow(IntPtr hWnd);
+}
+"@ 
+Add-Type -path $PSScriptRoot\user32.dll
+}
 
 #nugget: don't use -WindowStyle Hidden on the ps1 shortcut, it prevents retrieval of main MainWindowHandle here...
 #save MainWindowHandle so we can hide explicitly here and be available to show at end for troubleshooting IIF PowerShell errors were emitted
-$poShConsoleHwnd = (Get-Process -Id $pid).MainWindowHandle
+$process = Get-Process -Id $pid
+$poShConsoleHwnd = $process.MainWindowHandle
+if ($process.ProcessName -eq "powershell_ise") { $poShConsoleHwnd=0 }
 function showPoShConsole {
   param([bool]$show = $true)
   
-  #[System.Windows.Forms.MessageBox]::Show($poShConsoleHwnd) | Out-Null
-  [Win32]::ShowWindowAsync($poShConsoleHwnd, @([Win32]::SW_HIDE, [Win32]::SW_SHOWNORMAL)[$show]) | Out-Null
+  if ($show -and [User32]::IsWindowVisible($poShConsoleHwnd)) { $show=$false }
+
+  [User32]::ShowWindowAsync($poShConsoleHwnd, @([User32]::SW_HIDE, [User32]::SW_SHOWNORMAL)[$show]) | Out-Null
+  [User32]::SetForegroundWindow($poShConsoleHwnd) | Out-Null
 }
 
 showPoShConsole $false
@@ -176,26 +194,26 @@ $splitContainer_Resize =
 {
   if (!$tabContainerLeft -or !$tabContainerLeft.SelectedTab -or !$tabContainerLeft.SelectedTab.Tag.Hwnd) { return }
   $tab = $tabContainerLeft.SelectedTab
-  [Win32]::SetWindowPos(
+  [User32]::SetWindowPos(
     $tab.Tag.Hwnd,
-    [Win32]::HWND_TOP,
+    [User32]::HWND_TOP,
     $tab.ClientRectangle.Left,
     $tab.ClientRectangle.Top,
     $tab.ClientRectangle.Width,
     $tab.ClientRectangle.Height,
-    [Win32]::NOACTIVATE -bor [Win32]::SHOWWINDOW
+    [User32]::NOACTIVATE -bor [User32]::SHOWWINDOW
   ) | Out-Null
 
   if (!$tabContainerRight -or !$tabContainerRight.SelectedTab -or !$tabContainerRight.SelectedTab.Tag.Hwnd) { return }
   $tab = $tabContainerRight.SelectedTab
-  [Win32]::SetWindowPos(
+  [User32]::SetWindowPos(
     $tab.Tag.Hwnd,
-    [Win32]::HWND_TOP,
+    [User32]::HWND_TOP,
     $tab.ClientRectangle.Left,
     $tab.ClientRectangle.Top,
     $tab.ClientRectangle.Width,
     $tab.ClientRectangle.Height,
-    [Win32]::NOACTIVATE -bor [Win32]::SHOWWINDOW
+    [User32]::NOACTIVATE -bor [User32]::SHOWWINDOW
   ) | Out-Null
 }
 
@@ -223,7 +241,7 @@ function createButton {
 function closeTab {
   param([System.Windows.Forms.TabPage]$tabPage)
 
-  [Win32]::SendMessage($tabPage.Tag.Hwnd, [Win32]::WM_SYSCOMMAND, [Win32]::SC_CLOSE, 0) | Out-Null
+  [User32]::SendMessage($tabPage.Tag.Hwnd, [User32]::WM_SYSCOMMAND, [User32]::SC_CLOSE, 0) | Out-Null
   $tabPage.Tag.TabControl.TabPages.Remove($tabPage)
 }
 
@@ -235,10 +253,10 @@ function closeTab {
 #System.DirectoryServices.AccountManagement.UserPrincipal.Current.DisplayName would allow us to find window, but .Current was taking 5 seconds?!? presumably to timeout on finding a DC (on a non domain PC? braindead MS??)...
 #so found the GetUserNameEx Win32API approach
 $userDisplayName = New-Object System.Text.StringBuilder -ArgumentList 1024
-[Win32]::GetUserNameEx([int][Win32+EXTENDED_NAME_FORMAT]::NameDisplay, $userDisplayName, [ref] $userDisplayName.Capacity) | Out-Null #nugget: embedded C# enum syntax
+[User32]::GetUserNameEx([int][User32+EXTENDED_NAME_FORMAT]::NameDisplay, $userDisplayName, [ref] $userDisplayName.Capacity) | Out-Null #nugget: embedded C# enum syntax
 
 function newFileExTab {
-  param([bool]$leftSide)
+  param([bool]$leftSide, [string]$folderPath)
 
   $tabPage = New-Object System.Windows.Forms.TabPage
   $tabContainer = @($tabContainerRight, $tabContainerLeft)[$leftSide]
@@ -252,18 +270,20 @@ function newFileExTab {
 
   # if these windows class lookups change over Win.next, WinSpy tool is our friend:
   # http://www.catch22.net/software/winspy-17
-  do { $hwnd = [Win32]::FindWindow("CabinetWClass", [string]$userDisplayName) } while ( $hwnd -eq 0 )
+  do { $hwnd = [User32]::FindWindow("CabinetWClass", [string]$userDisplayName) } while ( $hwnd -eq 0 )
   #snag and save the individual "Window" interface (SHDocVw.InternetExplorer) for each of our File Explorers - to be used later for pulling the currently selected items in the CopyFile code
   do { $shDocVw = $shellWindows | ?{$_.HWND -eq $hwnd} } while ( !$shDocVw ) #my quick testing showed this would cycle 8 to 12 times before quiescing to a value
   
-  # good 'ol Win32 SetParent()
+  # good 'ol User32 SetParent()
   # i know it's silly but actually trying this right here with our old friend explorer.exe has been haunting me literally for years
-  [Win32]::SetParent($hwnd, $tabPage.Handle) | Out-Null
-  [Win32]::HideTitleBar($hwnd)
+  [User32]::SetParent($hwnd, $tabPage.Handle) | Out-Null
+  [User32]::HideTitleBar($hwnd)
 
   $tabPage.Tag = New-Object –TypeName PSObject -Property @{ ShDocVw=$shDocVw; Hwnd=$hwnd; TabControl=$tabContainer }
 
   $splitContainer_Resize.Invoke()
+
+  if ($folderPath) { @((rightShell), (leftShell))[$leftSide].Navigate($folderPath) }
 }
 
 
@@ -277,7 +297,10 @@ function leftShell { return $tabContainerLeft.SelectedTab.Tag.ShDocVw }
 function rightShell { return $tabContainerRight.SelectedTab.Tag.ShDocVw }
 function leftPath { return uriToWindowsPath (leftShell).LocationUrl }
 function rightPath { return uriToWindowsPath (rightShell).LocationUrl }
-function leftSelectedItems { return (leftShell).Document.SelectedItems() }
+function leftSelectedItems { (leftShell).Document.SelectedItems() }
+function rightSelectedItems { (rightShell).Document.SelectedItems() }
+function leftFirstSelectedPath { $items = leftSelectedItems; if ($items.Count -gt 0) { $items.Item(0).Path } }
+function rightFirstSelectedPath { $items = rightSelectedItems; if ($items.Count -gt 0) { $items.Item(0).Path } }
 
 function copyLeftToRight {
   param([bool]$move)
@@ -300,18 +323,15 @@ function copyLeftToRight {
   # i.e. one starts to see that as obtuse as all these interfaces seem at first, they do actually hang together
 
   # Folder.CopyHere - https://msdn.microsoft.com/en-us/library/windows/desktop/bb787866%28v=vs.85%29.aspx
-  @($rightFolder.CopyHere, $rightFolder.MoveHere)[$move].Invoke((leftSelectedItems))
+  @($rightFolder.CopyHere, $rightFolder.MoveHere)[$move].Invoke((leftShell).Document.SelectedItems()) #interesting, couldn't pass FolderItems collection as function result, would always only do first item
 }
 
 # https://msdn.microsoft.com/en-us/library/windows/desktop/bb787866%28v=vs.85%29.aspx
 $objShell = New-Object -ComObject "Shell.Application"
 
-Add-Type -Path $PSScriptRoot\Interop.SHDocVw.dll
-#i don't quite grok why we have to use SHDocVw.ShellWindows*Class* here in powershell vs just SHDocVw.ShellWindows in C#
-#but anyway, instantiating this interface gives us a collection of "SHDocVw.InternetExplorer" objects: https://msdn.microsoft.com/en-us/library/aa752084(v=vs.85).aspx
-#which is goofy since this has nothing to do with IE but that's obviously just the way MS baked the shared UI components together
-#apparently you could also obtain this "Windows" collection via (new Shell32.Shell).Windows()
-$shellWindows = New-Object "SHDocVw.ShellWindowsClass"
+#Shell.Application::Windows() gives us a collection of "SHDocVw.InternetExplorer" objects: https://msdn.microsoft.com/en-us/library/aa752084(v=vs.85).aspx
+#which is goofy naming since this has nothing to do with IE but that's obviously just the way MS baked the shared UI components together
+$shellWindows = $objShell.Windows()
 
 $splitContainer = new-object System.Windows.Forms.SplitContainer
 $splitContainer.Dock = [System.Windows.Forms.DockStyle]::Fill
@@ -348,6 +368,18 @@ $frmMain.Controls.Add($buttonPanel) | Out-Null
 
 newFileExTab $true
 newFileExTab $false
+
+$btn = createButton -toolTipText "Open Selected Folder New Tab RIGHT" -iconType ([FontAwesomeIcons.IconType]::FolderO) -eventHandler { newFileExTab $false (rightFirstSelectedPath) } 
+$btn.Dock = [System.Windows.Forms.DockStyle]::Left
+$buttonPanel.Controls.Add($btn)
+
+$btn = createButton -toolTipText "Open Selected Folder New Tab LEFT" -iconType ([FontAwesomeIcons.IconType]::FolderO) -eventHandler { newFileExTab $true (leftFirstSelectedPath) } 
+$btn.Dock = [System.Windows.Forms.DockStyle]::Left
+$buttonPanel.Controls.Add($btn)
+
+$btn = createButton -toolTipText "Show PowerShell Console" -iconType ([FontAwesomeIcons.IconType]::Terminal) -eventHandler { showPoShConsole } 
+$btn.Dock = [System.Windows.Forms.DockStyle]::Left
+$buttonPanel.Controls.Add($btn)
 
 $btn = createButton -toolTipText "Jam Right path to Left" -iconType ([FontAwesomeIcons.IconType]::LongArrowLeft) -eventHandler { (leftShell).Navigate((rightPath)) } 
 $btn.Dock = [System.Windows.Forms.DockStyle]::Left
@@ -395,4 +427,4 @@ $frmMain.add_FormClosing({
 
 [System.Windows.Forms.Application]::Run($frmMain)
 
-if ($Error) { showPoShConsole; pause }
+if ($Error -and $poShConsoleHwnd -ne 0) { showPoShConsole; pause }
